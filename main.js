2887dc93a307ebc4e5a54a27e26c3d5e0b69fd05
ly li  <lilili846244@gmail.com>
2023-07-26 00:27:33 +0800

    new-1

--- main.js
+++ main.js
@@ -14,6 +14,24 @@ gsap.to(".cover_background-h1",1, {
     }
   });
 
+  gsap.fromTo(".cover_background-image",
+  {
+    scale:1.2,
+  },{
+    scale:1,
+    ease: "power1.in",
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".cover_background-frame-wrapper",
+      start: "top 70%",
+      end: "30% 70%",
+      scrub: 1,
+      markers: {startColor: "white", endColor: "white", fontSize: "20px", fontWeight: "bold", indent: 20},
+      id: "scrub",}
+    },"-=1"
+  );
+
+
   gsap.from(".padding-section-large h1",1.5, {
     x:-100,
     opacity:0,
@@ -23,13 +41,13 @@ gsap.to(".cover_background-h1",1, {
       start: "top 75%",
       end: "80% 70%",
       scrub: 1,
-      markers: true,
+      markers: {startColor: "green", endColor: "green", fontSize: "24px"},
       id: "scrub"
     }
   });
 
   gsap.to(".cover_background-h1",1, {
-    opacity: 1,
+    opacity: 0.9,
     ease: "power1.out",
     scrollTrigger: {
       trigger: ".cover_background-frame-wrapper",
